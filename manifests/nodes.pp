d55928288a000d664547723072e1fe733a46301a
Thibault CORDIER <thibault.cordier@gmail.com>
2012-05-31 14:39:34 +0200

    Local modification, don't care...

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,21 +1,16 @@
 node default {
 	include ntp
 }
-node 'test2.db-box.fr' inherits default {
-}
-node 'superbox.db-box.fr' inherits default {
-	include netatalk
+node 'master.db-box.fr' inherits default {
+		include netatalk
 		include avahi
 		include firewalldbbox
 		include acl
 		include samba
 		include apache
 		include phpmyadmin
-		include backuppc
+#		include backuppc
 		include webdav
 		include concat::setup
 		include nginx
-
-
 }
-
